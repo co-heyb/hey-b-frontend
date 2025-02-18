8ae5d7563d56ae8de89d38e4a569d8e62261bcec
jy.kim <kjy93k@naver.com>
2025-02-18 14:59:10 +0900

    메타태그 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,29 +7,37 @@ import 'pretendard/dist/web/variable/pretendardvariable-dynamic-subset.css';
 import { container } from './layout.css';
 
 export const metadata: Metadata = {
-  title: 'HeyB - 나만의 취미를 발견하고 즐기는 플랫폼',
+  title: 'HeyB - 작은 시작이 만드는 새로운 즐거움',
   description:
-    'HeyB에서 나만의 취미를 찾아보세요. 체험 키트로 시작하고, 경험을 공유하며 즐거움을 나눌 수 있는 취미 플랫폼! 취미를 탐색하고, 나누고, 성장하세요.',
+    '아무것도 안 해도 괜찮아요. 하지만, 가볍게 시작하면 더 즐거워질 거예요. 취미를 찾고, 작은 도전을 해보고, 함께 경험을 나누세요. 혼자보다는 같이, 더 나은 나를 만들어가는 곳. HeyB에서 당신의 변화를 만나보세요.',
   icons: {
     icon: '/favicon.ico',
   },
   keywords: [
+    '작은 도전',
+    '목표',
     '취미',
     '취미 플랫폼',
+    'DIY',
     '체험 키트',
     '취미 공유',
     '취미 추천',
-    'DIY',
     '나만의 취미',
-    'HeyB',
+    '습관 만들기',
+    '새로운 시작',
+    '커뮤니티',
     '취미 커뮤니티',
+    '도전',
+    '응원',
+    '변화',
     '성장',
+    'HeyB',
   ],
   // authors: [{ name: "HeyB Team", url: "https://www.heyb.com" }],
   openGraph: {
-    title: 'HeyB - 나만의 취미를 발견하고 즐기는 플랫폼',
+    title: 'HeyB - 작은 시작이 만드는 새로운 즐거움',
     description:
-      'HeyB에서 나만의 취미를 찾아보세요. 체험 키트로 시작하고, 경험을 공유하며 즐거움을 나눌 수 있는 취미 플랫폼! 취미를 탐색하고, 나누고, 성장하세요.',
+      '아무것도 안 해도 괜찮아요. 하지만, 가볍게 시작하면 더 즐거워질 거예요. 취미를 찾고, 작은 도전을 해보고, 함께 경험을 나누세요. 혼자보다는 같이, 더 나은 나를 만들어가는 곳. HeyB에서 당신의 변화를 만나보세요.',
     // url: "https://www.heyb.com",
     siteName: 'HeyB',
     locale: 'ko_KR',
